01e7e0d8ceb3d1d9a366dcaa3f3cfdb4716bbe05
Dilasnaofficical <98165475+Dilasnaofficical@users.noreply.github.com>
2022-02-11 11:36:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Your Bot token in the quotes taken from @BotFather
+BOT_TOKEN = "5153139827:AAF4TADZCl37OF1yitSCqiP3UP4o2pZ5X2g"
+
+# Project Name of your Project
+PROJECT_NAME = "GitGram"
+
+# If you forked the source code, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
